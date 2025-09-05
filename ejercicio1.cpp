c9dc743770f618217fe2104977eff5f37f677d01
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2025-09-05 10:19:41 -0300

    Constructor NodoAVL

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,6 +5,36 @@
 
 using namespace std;
 
+class NodoAVL
+{
+private:
+    NodoAVL *izq;
+    NodoAVL *der;
+    int id;
+    string nombre;
+    int puntaje;
+
+public:
+    NodoAVL(int idJugador, string nombreJugador, int puntajeJugador)
+    {
+        izq = NULL;
+        der = NULL;
+        id = idJugador;
+        nombre = nombreJugador;
+        puntaje = puntajeJugador;
+    }
+    ~NodoAVL()
+    {
+
+    }
+};
+
+/*class ArbolAVL{
+
+}
+*/
+    
+
 int main()
 {
     return 0;
