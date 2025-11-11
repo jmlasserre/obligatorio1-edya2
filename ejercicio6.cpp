a86b0f3e461f6a936ea59456d17ea9f86da8b3f8
Juan Martín Lasserre <JL289139@fi365.ort.edu.uy>
2025-11-10 21:51:28 -0300

    temp ej6, incompleto

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -8,11 +8,132 @@
 #include <string>
 #include <iostream>
 #include <limits>
-
+#include "tads/ListImp.cpp"
+#include "tads/MinHeap.cpp"
+#include "tads/MaxHeap.cpp"
 using namespace std;
+
+struct Pozo
+{
+    int inicio, final;
+    int getInicio() { return this->inicio; }
+    int getFinal() { return this->final; }
+    Pozo() : inicio(-1), final(-1) {}
+    Pozo(int inicio, int final) : inicio(inicio), final(final) {}
+    bool operator==(const Pozo &otro)
+    {
+        return this->inicio == otro.inicio && this->final == otro.final;
+    }
+};
+
+struct Mejora
+{
+    int pos, valor;
+    int getPos() { return this->pos; }
+    int getValor() { return this->valor; }
+    Mejora() : pos(-1), valor(-1) {}
+    Mejora(int pos, int valor) : pos(pos), valor(valor) {};
+    bool operator==(const Mejora &otro) const
+    {
+        return this->valor == otro.valor;
+    }
+
+    bool operator<(const Mejora &otro) const
+    {
+        return this->valor < otro.valor;
+    }
+
+    bool operator>(const Mejora &otro) const
+    {
+        return this->valor > otro.valor;
+    }
+    bool operator>=(const Mejora &otro) const{
+        return this->valor >= otro.valor;
+    }
+};
 
 int main()
 {
-    // TODO
+    int N, M, F;
+    cin >> N >> M >> F;
+    //MinHeap<Mejora> *mejoras = new MinHeap<Mejora>(M);
+    ListImp<Pozo> *pozos = new ListImp<Pozo>();
+
+    for (int i = 0; i < N; i++)
+    {
+        int inicio, fin;
+        cin >> inicio >> fin;
+        pozos->insert(Pozo(inicio, fin));
+    }
+    /*for (int i = 0; i < M; i++)
+    {
+        int direccion, mejora;
+        cin >> direccion >> mejora;
+        mejoras->insertar(Mejora(direccion, mejora));
+    }
+
+    while (!mejoras->esVacio()){
+        Mejora m = mejoras->eliminarMin();
+        cout << "(" << m.getPos() << ", " << m.getValor() << ")" << endl;
+    }*/
+    MaxHeap<Mejora>* inventario = new MaxHeap<Mejora>(M);
+    int minMejoras = 0, potencia = 1, pos = 1, posMaxMejora = 0, maxMejora = 0, maxPos = 0, pozoActual = 0, cantMejoras=0;
+    int movs = 1; // eliminar
+    while (!pozos->isEmpty())
+    {
+        int rango = pos + potencia;
+        if (rango >= F)
+        {
+            cout << minMejoras << endl;
+            return 0;
+        }
+        Pozo p = pozos->get(0);
+        int inicio = p.getInicio();
+        int final = p.getFinal();
+        cout << "Debug: Inicio del pozo: " << inicio << endl;
+        cout << "Debug: Final del pozo: " << final << endl;
+        if (rango > final)
+        {
+            pos = final + 1; // hay al menos un espacio entre pozos
+            pozos->removeAt(0);
+        }
+        else
+        {
+            // si está en un agujero, recién ahí empiezo a agarrar mejoras
+            int posTemp = inicio-1;
+            while (cantMejoras < M){
+                int pos, valor;
+                cin >> pos >> valor;
+                cout << "Debug: posTemp = " << posTemp << endl;
+                if (pos > posTemp) break; // está más allá de mi alcance, no la puedo acceder
+                inventario->insertar(Mejora(pos, valor));
+                cantMejoras++;
+            }
+            while (!inventario->esVacio() && (posTemp + potencia) <= final)
+            {
+                Mejora m = inventario->eliminarMax();
+                cout << "Debug: Valor de la mejora: " << m.getValor() << endl;
+                potencia += m.getValor();
+                minMejoras++;
+            }
+            cout << "Debug: Potencia actual: " << potencia << endl;
+            cout << "Debug: Nuevo rango: " << posTemp + potencia << endl;
+            if (posTemp + potencia <= final)
+            { // agarré todas las mejoras y no lo pude pasar, no se puede terminar.
+                cout << "Imposible" << endl;
+                return 0;
+            }
+            else
+            {
+                pos = final + 1;
+                pozos->removeAt(0);
+            }
+        }
+    }
+    cout << minMejoras << endl;
     return 0;
 }
+
+// NOTA: PENSAR DE ESTA MANERA:
+// a medida que voy avanzando, voy consiguiendo mejoras. ESAS mejoras van en un MaxHeap para así ir sacando
+// siempre la más grande antes. Habría que ver cómo hacer esta parte. Después la lógica es similar.
---
+++ tads/MaxHeap.cpp
@@ -0,0 +1,85 @@
+#ifndef MAX_HEAP
+#define MAX_HEAP
+
+template <class T>
+class MaxHeap
+{
+private:
+    int sizeActual;
+    int max;
+    T* heap;
+
+    int padre(int index) { return (index - 1) / 2; }
+    int hijoIzq(int index) { return 2 * index + 1; }
+    int hijoDer(int index) { return 2 * index + 2; }
+
+    void flotar(int index)
+    {
+        while (index > 0 && heap[index] > heap[padre(index)])
+        {
+            swap(index, padre(index));
+            index = padre(index);
+        }
+    }
+
+    void hundir(int index)
+    {
+        while (hijoIzq(index) < sizeActual)
+        {
+            int j = hijoIzq(index);
+            if (heap[j] < heap[j + 1])
+                j++;
+            if (heap[index] >= heap[j])
+                break;
+            swap(index, j);
+            index = j;
+        }
+    }
+
+    void swap(int a, int b)
+    {
+        T temp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = temp;
+    }
+
+public:
+    MaxHeap(int capacidad)
+    {
+        sizeActual = 0;
+        max = capacidad;
+        heap = new T[capacidad];
+    }
+    ~MaxHeap()
+    {
+        delete[] heap;
+    }
+
+    bool esVacio() { return sizeActual == 0; }
+
+    int getLargo() { return sizeActual; }
+
+    void insertar(T e)
+    {
+        if (sizeActual == max)
+            return;
+        heap[sizeActual] = e;
+        flotar(sizeActual++);
+    }
+
+    T eliminarMax()
+    {
+        if (esVacio())
+            return T();
+        T max = heap[0];
+        heap[0] = heap[--sizeActual];
+        hundir(0);
+        return max;
+    }
+
+    T getMax(){
+        return heap[0];
+    }
+};
+
+#endif
